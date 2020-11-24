4c594409218a7287e436e5ffd3958ac536ac7232
Giacomo <giacomo.1936@gmail.com>
2020-11-24 13:00:36 +0100

    modifica

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ var app = new Vue ({
     current_genre: '',
     album_corrente: 0,
     data: {
+        //imposto una lista_album dove tramite api assegnerò tutti i dati di ogni rispettivo album
+        lista_album: [
 
+        ]
     },
+
+    //a pagina caricata prendo l'api dove ho i dati da importare
     mounted() {
         const self = this;
-        axios.get()
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(function(risposta) {
+                //assegno alla lista_album i dati dell'album che poi ciclerò attraverso un v-for
                 self.lista_album = risposta.data.response;
             });
     }
