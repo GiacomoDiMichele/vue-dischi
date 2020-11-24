6a3368bc18595b94ff90c9816307f0332008fd5d
Giacomo <giacomo.1936@gmail.com>
2020-11-24 12:54:58 +0100

    impostata barra di ricerca ed header

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 //creo il collegamento vue
 var app = new Vue ({
     el: '#root',
+    current_genre: '',
+    album_corrente: 0,
     data: {
-        //imposto una lista_album dove tramite api assegnerò tutti i dati di ogni rispettivo album
-        lista_album: [
 
-        ]
     },
-
-    //a pagina caricata prendo l'api dove ho i dati da importare
     mounted() {
         const self = this;
-        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        axios.get()
             .then(function(risposta) {
-                //assegno alla lista_album i dati dell'album che poi ciclerò attraverso un v-for
                 self.lista_album = risposta.data.response;
             });
     }
